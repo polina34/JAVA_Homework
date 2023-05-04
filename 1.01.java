7ec07d26693655fcacddacee626254140520dd92
Polina <polyamsk@yandex.ru>
2023-05-04 20:50:07 +0400

    start

---
+++ 1.01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TriangleFactorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число n: ");
+        int n = scanner.nextInt();
+
+        // Вычисляем n-ое треугольное число
+        int triangleNumber = 0;
+        for (int i = 1; i <= n; i++) {
+            triangleNumber += i;
+        }
+        System.out.println("n-ое треугольное число: " + triangleNumber);
+
+        // Вычисляем n!
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println("n!: " + factorial);
+    }
+}
